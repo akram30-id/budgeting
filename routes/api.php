9895d14f9fa8b6337a3468982b8096a4735d7795
akram30-id <afalerin2016@gmail.com>
2025-10-26 10:05:02 +0700

    add: comment to trigger cd

--- routes/api.php
+++ routes/api.php
@@ -19,9 +19,12 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+// REGISTER & LOGIN
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
+
+// AUTHENTICATED ROUTES
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/me', [AuthController::class, 'me']);
     Route::post('/logout', [AuthController::class, 'logout']);
